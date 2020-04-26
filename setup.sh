272fb5a2e1c5f37d7d786991625dd5c9bcdfd922
Jeff Lindsay <progrium@gmail.com>
2020-04-25 21:11:04 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install mas git go ffmpeg rust jq wget youtube-dl make python
+brew cask install google-chrome
+brew cask install visual-studio-code
+brew cask install spotify
+brew cask install textmate
+brew cask install vlc
+brew cask install dropbox
+brew cask install discord
+brew cask install 1password
+brew cask install docker
+brew cask install unity-hub
+brew cask install shiftit
+brew cask install adobe-creative-cloud
+
