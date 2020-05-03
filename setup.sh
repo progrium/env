b9891841b64b26f9cf4ac55de1ea5b7175955ac4
Jeff Lindsay <progrium@gmail.com>
2020-05-02 20:17:03 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 tmutil localsnapshot
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew install mas git go dockutil ruby
+sudo softwareupdate --fetch-full-installer
+brew install mas git go dockutil ruby brew-gem
 #brew install ffmpeg rust jq wget youtube-dl make python
 brew cask install google-chrome
 brew cask install visual-studio-code
@@ -16,6 +17,8 @@ brew cask install docker
 brew cask install unity-hub
 brew cask install shiftit
 #brew cask install adobe-creative-cloud
+
+brew gem install pry
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 chmod g-w,o-w /usr/local/share/zsh
